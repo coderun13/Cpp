f69087fe8b4ace55e6591e920f647fcb47c478b6
coderun13 <snehalsingh.0407@gmail.com>
2024-12-29 10:22:53 +0530

    add new file

---
+++ DSA_Concepts/Graph/Representation Methods/Adjacency_Matrix/Directed_weighted_graph.cpp
@@ -0,0 +1,46 @@
+/** 
+ * Work Flow
+ * make int fucntion and take input
+ * make matrix 
+ * use for loop
+ * and print the matrix 
+ */
+
+/**
+ * Undirected weighted graph
+ * Vertex are 5 (0,1,2,3,4)
+ * The edges are from (0,1)(1,0)(1,2)(2,1)(0,2)(2,0)(4,3)(3,4)
+ * Weight is 11,12,13,14,15,16
+ */
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int vertex, edges;
+    cout<<"Enter the number of vertex: ";
+    cin>>vertex;
+    cout<<"Enter the number of edges: ";
+    cin>>edges;
+
+    vector<vector<int>>AdjMat(vertex, vector<int>(vertex,0));
+
+    int u,v,weight;
+    for (int i = 0;i<edges; i++)
+    {
+        cout<<"Enter the value of u and v and weight in matrix: ";
+        cin>>u>>v>>weight;
+        AdjMat[u][v]= weight;
+    }
+
+    for(int i = 0; i<vertex;i++)
+    {
+        for(int j = 0; j<vertex;j++)
+        {
+            cout<<AdjMat[i][j]<<" ";
+
+        }
+          cout<<endl;
+    }
+}
