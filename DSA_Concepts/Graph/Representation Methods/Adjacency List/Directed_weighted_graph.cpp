1471206aab38e0e39ea5a73d29581190f7092b5c
coderun13 <snehalsingh.0407@gmail.com>
2024-12-29 10:18:51 +0530

    add new file

---
+++ DSA_Concepts/Graph/Representation Methods/Adjacency List/Directed_weighted_graph.cpp
@@ -0,0 +1,40 @@
+/**
+ * Work Flow
+ * make main function and take input for vertex and edges
+ * Create vector
+ * Take input for u, v and weight in array 
+ * v, weight in u(create edges)
+ * print the edge is from where
+ */
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int edges, vertex;
+    cout<<"Enter the value of vertex: ";
+    cin>>vertex;
+    cout<<"Enter the number of edges: ";
+    cin>>edges;
+
+    vector<pair<int,int>>AdjList[vertex];
+
+    int u, v, weight;
+    for(int i = 0; i<edges;i++)
+    {
+        cout<<"enter the value of u, v and weight in array: ";
+        cin>>u>>v>>weight;
+        AdjList[u].push_back(make_pair(v, weight));
+    }
+
+    for (int i = 0; i<vertex;i++)
+    {
+        cout<<"The edge is from: "<<i<<" -> ";
+            for(int j = 0; j<AdjList[i].size();j++)
+            {
+                cout<<"("<<AdjList[i][j].first<<" "<<AdjList[i][j].second<<")"<<" ";
+            }
+                cout<<endl;
+    }
+}
---
+++ DSA_Concepts/Graph/Representation Methods/Adjacency List/Undirected_unweighted_graph.cpp
@@ -0,0 +1,41 @@
+/**
+ * Work Flow
+ * make main function and take input for vertex and edges
+ * Create vector
+ * Take input for u and v in array 
+ * push u in v and v in u(create edges)
+ * print the edge is from where
+ */
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int edges, vertex;
+    cout<<"Enter the value of vertex: ";
+    cin>>vertex;
+    cout<<"Enter the number of edges: ";
+    cin>>edges;
+
+    vector<int>AdjList[vertex];
+
+    int u, v;
+    for(int i = 0; i<edges;i++)
+    {
+        cout<<"enter the value of u and v in array: ";
+        cin>>u>>v;
+        AdjList[u].push_back(v);
+        AdjList[v].push_back(u);
+    }
+
+    for (int i = 0; i<vertex;i++)
+    {
+        cout<<"The edge is from: "<<i<<" -> ";
+            for(int j = 0; j<AdjList[i].size();j++)
+            {
+                cout<<AdjList[i][j]<<" ";
+            }
+                cout<<endl;
+    }
+}
