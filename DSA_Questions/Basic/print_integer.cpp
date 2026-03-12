2ed067381a86bfec3dfb33d1e3b27e6badd23c5c
Snehal Singh <snehalsingh.0407@gmail.com>
2026-03-13 01:01:06 +0530

    new question solved

---
+++ DSA_Questions/Basic/print_integer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Integers from 1-10 are:" << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+}
