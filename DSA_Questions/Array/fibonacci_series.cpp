f8e4872b7d268a5cead7ee3d09dfc36f614c8ada
Snehal Singh <snehalsingh.0407@gmail.com>
2026-03-15 18:36:03 +0530

    file modified

--- DSA_Questions/Array/fibonacci_series.cpp
+++ DSA_Questions/Array/fibonacci_series.cpp
@@ -1,21 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//series -> 0,1,1,2,3,5,8,13...
+// series -> 0,1,1,2,3,5,8,13...
 
-int main(){
-    int n;
-    cout<<"Enter the position: ";
-    cin>>n;
-    int arr[1000];
-
-    //first =0;
-    //second=1;
-    arr[0]=0;
-    arr[1]=1;
-    
-    for(int i=2;i<=n-1;i++)
-    arr[i]= arr[i-1]+arr[i-2];
-
-    cout<<"The number in that position in the series is: "<<arr[n-1];
+int main()
+{
+    int n1 = 0, n2 = 1, n3, n;
+    cout << "Enter the number till where you want the series: ";
+    cin >> n;
+    cout << n1 << "," << n2;
+    for (int i = 1; i <= n; i++)
+    {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        cout << "," << n3;
+    }
 }
+
+// int main(){
+//     int n;
+//     cout<<"Enter the position: ";
+//     cin>>n;
+//     int arr[1000];
+
+//     //first =0;
+//     //second=1;
+//     arr[0]=0;
+//     arr[1]=1;
+
+//     for(int i=2;i<=n-1;i++)
+//     arr[i]= arr[i-1]+arr[i-2];
+
+//     cout<<"The number in that position in the series is: "<<arr[n-1];
+// }
