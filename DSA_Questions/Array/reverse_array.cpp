d4be80093a67c106c453068de60b3c6cdda4ae45
Snehal Singh <snehalsingh.0407@gmail.com>
2026-03-18 21:43:45 +0530

    file modified

--- DSA_Questions/Array/reverse_array.cpp
+++ DSA_Questions/Array/reverse_array.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-int main(){
+int main()
+{
 
-int arr[5]={111,33,48,55,6};
-int i,j,temp[5];
- i=5, i>=0;
- j=0,j<=5;
-while(i<j){
-    temp[j]= arr[i];
-    i--,j++;
-};
-cout<<"The given array is: "<<arr[i]<<endl;
-cout<<"The reverse array is: "<<temp[j];
-
+    int arr[5] = {111, 33, 48, 55, 6};
+    int i, j, temp[5];
+    i = 5, i >= 0;
+    j = 0, j <= 5;
+    while (i < j)
+    {
+        temp[j] = arr[i];
+        i--, j++;
+    };
+    cout << "The given array is: " << arr[i] << endl;
+    cout << "The reverse array is: " << temp[j];
 }
+
+// array reverse
