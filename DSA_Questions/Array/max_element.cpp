56ac08f124ecb0d783c1c8de3a253719274a384e
snehal singh <snehalsingh.0407@gmail.com>
2026-04-10 23:47:50 +0530

    new question

--- DSA_Questions/Array/max_element.cpp
+++ DSA_Questions/Array/max_element.cpp
@@ -1,16 +1,43 @@
+// #include<iostream>
+// using namespace std; 
+
+// int main(){
+
+// int arr[5]={111,33,48,55,6};
+// int i;
+// int ans = arr[0];
+// for(i=0; i<5; i++){
+//     if(arr[i]> ans){
+//         ans = arr[i];
+//     }
+// }
+//     cout<<"The largest element is: "<<ans;
+//     return 0;
+// }
+
+
 #include<iostream>
-using namespace std; 
+using namespace std;
 
-int main(){
+int main()
+{
+    int n, max=0;
+    cout<<"Enter the size: ";
+    cin>>n;
 
-int arr[5]={111,33,48,55,6};
-int i;
-int ans = arr[0];
-for(i=0; i<5; i++){
-    if(arr[i]> ans){
-        ans = arr[i];
+    int arr[n];
+    cout<<"Enter the array elements: ";
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-}
-    cout<<"The largest element is: "<<ans;
-    return 0;
+    
+    max = arr[0];
+    
+    for(int i=0;i<n;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+    }
+    cout<<"Max element is: "<<max;
 }
