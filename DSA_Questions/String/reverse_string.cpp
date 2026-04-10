5150bbb4848b3c71469ad605ac4f0e70919bfb95
snehal singh <snehalsingh.0407@gmail.com>
2026-04-11 00:36:04 +0530

    new question

---
+++ DSA_Questions/String/reverse_string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    char str[] = "snehal";
+    //method
+    // cout<<strrev(str);
+
+    //iteration
+    int length = strlen(str);
+    for(int i=length-1;i>=0;i--)
+    {
+        cout<<str[i];
+    }
+}
